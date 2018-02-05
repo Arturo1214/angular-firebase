504e5ea90f7c2f70a0ab67e55737be2858adeadd
Arturo Herrera O <arturoherreraocana@gmail.com>
2018-02-05 00:22:54 -0400

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {ProveedoresService} from './servicios/proveedores.service';
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import {Routes, RouterModule} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+import {FormsModule} from '@angular/forms';
 
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'proveedores', component: ProveedoresComponent},
+  {path: 'addprovee', component: AddproveeComponent},
+  {path: '**', component: InicioComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent,
+    AddproveeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ProveedoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
